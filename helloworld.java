d16e42f2b7e557399fa688d3ab09f659ea0aedb2
shritej1808 <151450693+shritej1808@users.noreply.github.com>
2024-03-24 23:01:29 +0530

    lknkssa

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("\nHellooo\n");
+        System.out.println("\nthis is first java\n");
+    }
+}
